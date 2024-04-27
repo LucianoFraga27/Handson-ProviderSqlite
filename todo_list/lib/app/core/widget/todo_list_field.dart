223d706a09e5cdf22efee88da43221c91df680f1
Lucas Silva <lucas.silva@abramus.org.br>
2024-04-27 11:04:31 -0300

    M3 pt2
    utlizando value Notifier para pegar estado do olho

--- todo_list/lib/app/core/widget/todo_list_field.dart
+++ todo_list/lib/app/core/widget/todo_list_field.dart
@@ -6,36 +6,46 @@ class TodoListField extends StatelessWidget {
   final IconButton? suffixIconButton;
   final Icon? suffixIcon;
   final bool obscureText;
-  
-  const TodoListField({ super.key, required this.label, this.obscureText=false, this.suffixIcon, this.suffixIconButton })
-  : assert ( obscureText == true ? suffixIconButton == null : true, "Obscure Text não pode ser enviado em conjunto com suffix icon button");
-  
 
-   @override
-   Widget build(BuildContext context) {
-       return TextField(
-        decoration: InputDecoration(
-          labelText: label,
-          labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30)
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30),
-            borderSide: const BorderSide(
-              color: Colors.red
-            )
-          ),
-          isDense: true,
-          suffixIcon: suffixIcon ??
-          (obscureText == true ? IconButton(onPressed: (){
+  final ValueNotifier<bool> obscureTextVM;
 
+  TodoListField(
+      {Key? key,
+      required this.label,
+      this.obscureText = false,
+      this.suffixIcon,
+      this.suffixIconButton})
+      : assert(obscureText == true ? suffixIconButton == null : true,
+            "Obscure Text não pode ser enviado em conjunto com suffix icon button"),
+        obscureTextVM = ValueNotifier(obscureText),
+        super(key: key);
 
-
-          },icon: const Icon(TodoListIcons.eye, size: 15)) : null)
-        ),
-        obscureText: obscureText,
-        
-       );
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVM,
+      builder: (_, obscureTextVALUE, child) {
+        return TextField(
+          decoration: InputDecoration(
+              labelText: label,
+              labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+              errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: Colors.red)),
+              isDense: true,
+              suffixIcon: suffixIcon ??
+                  (obscureText == true
+                      ? IconButton(
+                          onPressed: () {
+                            obscureTextVM.value = !obscureTextVALUE;
+                          },
+                          icon: Icon( !obscureTextVALUE ? TodoListIcons.eye_slash : TodoListIcons.eye, size: 15))
+                      : null)),
+          obscureText: obscureTextVALUE,
+        );
+      },
+    );
   }
 }
