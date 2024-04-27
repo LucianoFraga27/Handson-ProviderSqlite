56e472bac4688c9dd3693badad49bd3cf785023c
Lucas Silva <lucas.silva@abramus.org.br>
2024-04-26 23:20:23 -0300

    M3 pt1
    botão login com google

--- todo_list/lib/app/modules/auth/login/login_page.dart
+++ todo_list/lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:todo_list/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
@@ -41,7 +43,10 @@ class LoginPage extends StatelessWidget {
                                 onPressed: () {},
                                 child: Padding(
                                   padding: const EdgeInsets.all(10.0),
-                                  child: Text("Login", style: TextStyle(color: Colors.white),),
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
                                 ),
                                 style: ElevatedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
@@ -55,22 +60,34 @@ class LoginPage extends StatelessWidget {
                     ),
                     const SizedBox(height: 20),
                     Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
+                        child: Container(
+                      decoration: BoxDecoration(
                           color: const Color(0xffF0F3F7),
-                          border: Border(top: BorderSide(
-                            width: 2,
-                            color: Colors.grey.withAlpha(50)
-                          ))
-                        ),
-                        child: Column (
-                          children: [
-                            const SizedBox(height: 30),
-                            Container()
-                          ],
-                        ),
-                      )),
-                  
+                          border: Border(
+                              top: BorderSide(
+                                  width: 2, color: Colors.grey.withAlpha(50)))),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 30),
+                          SignInButton(Buttons.Google,
+                              padding: const EdgeInsets.all(5),
+                              shape: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(30)),
+                              text: "Continue com o Google",
+                              onPressed: () {}),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("Não tem conta?"),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text("Cadastre-se"))
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
                   ],
                 ),
               ),
