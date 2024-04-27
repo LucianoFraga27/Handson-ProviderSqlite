9bda9348b11a8a33b0648c553fec06950aef9ea7
Lucas Silva <lucas.silva@abramus.org.br>
2024-04-27 11:37:54 -0300

    M3 pt3
    estrutura da tela

--- todo_list/lib/app/modules/auth/register/register_page.dart
+++ todo_list/lib/app/modules/auth/register/register_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/ui/theme_extensions.dart';
 
 class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
 
-  const RegisterPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Register'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Todo List", style: TextStyle(fontSize: 10, color: context.primaryColor)),
+            Text("Cadastro", style: TextStyle(fontSize: 14, color: context.primaryColor))
+          ],
+        ),
+        automaticallyImplyLeading:
+            false, // isso remove o botão de voltar da app bar por padrão
+        leading: IconButton(
+          icon: ClipOval(
+            child: Container(
+                color: context.primaryColor.withAlpha(20),
+                padding: const EdgeInsets.all(8),
+                child: Icon(
+                  Icons.arrow_back_ios_outlined,
+                  size: 20,
+                  color: context.primaryColor,
+                )),
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
