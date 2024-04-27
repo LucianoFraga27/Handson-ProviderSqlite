010b73d81a0ff635d30324693dc16af8fc3a346d
Lucas Silva <lucas.silva@abramus.org.br>
2024-04-26 22:26:05 -0300

    M3 pt1
    Estrutura básica da tela de login

--- todo_list/lib/app/modules/auth/login/login_page.dart
+++ todo_list/lib/app/modules/auth/login/login_page.dart
@@ -2,33 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:todo_list/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  const LoginPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           body: LayoutBuilder(
-            builder: (context, constraint) {
-              return SingleChildScrollView(
-                child: ConstrainedBox (
-                  constraints: BoxConstraints(
-                    minHeight: constraint.maxHeight,
-                    minWidth: constraint.maxHeight
-                  ),
-                  child: const IntrinsicHeight( // O IntrinsicHeight permite que o column seja de qualquer tamanho que a tela acompanhará 
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                         SizedBox(height: 10),
-                         TodoListLogo()
-                      ],
-                    ),
-                  ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraint) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  minHeight: constraint.maxHeight,
+                  minWidth: constraint.maxHeight),
+              child: IntrinsicHeight(
+                // O IntrinsicHeight permite que o column seja de qualquer tamanho que a tela acompanhará
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 10),
+                    const TodoListLogo(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      child: Form(
+                          child: Column(
+                        children: [
+                          TextFormField(),
+                          const SizedBox(height: 20),
+                          TextFormField(),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(onPressed: (){}, child: const Text("Esqueceu sua senha?")),
+                              ElevatedButton(onPressed: () {}, child: const Text("Login"))
+                            ],
+                          )
+                        ],
+                      )),
+                    )
+                  ],
                 ),
-              );
-            },
-           ),
-       );
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
