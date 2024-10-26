ce907429234c2a8a27cd059f650b018e35f69db7
lucianolucas <luciano.lucas@bllc.com.br>
2024-10-26 14:39:12 -0300

    M5
    tentando rodar o projeto, lib/app/core/widget/todo_list_logo.dart:15:54: Error: The getter 'headline6' isn't defined for the class 'TextTheme'.
     - 'TextTheme' is from 'package:flutter/src/material/text_theme.dart' ('/C:/devPrograms/flutter/packages/flutter/lib/src/material/text_theme.dart').
    Try correcting the name to the name of an existing getter, or defining a getter or field named 'headline6'.
              Text("Todo List", style: context.textTheme.headline6)

--- todo_list/lib/app/core/ui/todo_list_ui_config.dart
+++ todo_list/lib/app/core/ui/todo_list_ui_config.dart
@@ -9,7 +9,7 @@ class TodoListUiConfig {
     primaryColorLight: const Color(0xffABC8F7),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        primary: const Color(0xff5C77CE),
+        // primary: const Color(0xff5C77CE),
       )
     )
   );
