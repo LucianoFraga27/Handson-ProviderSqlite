89d6f3044d0f73651019f4fbaf083cb5f9d7438f
lucianolucas <luciano.lucas@bllc.com.br>
2024-10-26 14:54:01 -0300

    M5
    criando classe utilitaria

---
+++ todo_list/lib/app/core/ui/messages.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/ui/theme_extensions.dart';
+
+class Messages {
+  final BuildContext context;
+
+  Messages._(this.context);
+
+  factory Messages.of(BuildContext context) {
+    return Messages._(context);
+  }
+
+  void showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    ));
+  }
+
+  void showInfo(String message) {
+    showMessage(message, Colors.red);
+  }
+
+  void showSuccess(String message) {
+    showMessage(message, context.primaryColor);
+  }
+
+  void showMessage(String message, Color color) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      backgroundColor: color
+    ));
+  }
+
+
+}
