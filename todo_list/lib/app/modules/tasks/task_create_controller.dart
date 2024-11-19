2e4e6a4aeb204a0c3ad88313ae04a71f30525a7b
lucianolucas <luciano.lucas@bllc.com.br>
2024-11-19 17:58:00 -0300

    M7 pt2 cadastro

--- todo_list/lib/app/modules/tasks/task_create_controller.dart
+++ todo_list/lib/app/modules/tasks/task_create_controller.dart
@@ -1,6 +1,38 @@
-
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
 import 'package:todo_list/app/core/notifier/default_change_notifier.dart';
+import 'package:todo_list/app/services/tasks/task_service.dart';
 
 class TaskCreateController extends DefaultChangeNotifier {
-  
+  TaskService _taskService;
+  DateTime? _selectedDate;
+
+  TaskCreateController({required TaskService taskService})
+      : _taskService = taskService;
+
+  set selectedDate(DateTime? selectedDate) {
+    resetState();
+    _selectedDate = selectedDate;
+    notifyListeners();
+  }
+
+  DateTime? get selectedDate => _selectedDate;
+
+  void save(String description) async {
+    try {
+      showLoadingAndResetState();
+      notifyListeners();
+      if (_selectedDate != null) {
+        await _taskService.save(_selectedDate!, description);
+        success();
+      } else {
+        setError("Data da Tarefa não selecionada");
+      }
+    } on Exception catch (e) {
+      print(e);
+      setError("Erro ao cadastrar Tarefa");
+    } finally {
+      hideLoading();
+      notifyListeners();
+    }
+  }
 }
--- todo_list/lib/app/modules/tasks/tasks_module.dart
+++ todo_list/lib/app/modules/tasks/tasks_module.dart
@@ -1,20 +1,26 @@
-
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_module.dart';
 import 'package:todo_list/app/modules/tasks/task_create_controller.dart';
 import 'package:todo_list/app/modules/tasks/task_create_page.dart';
+import 'package:todo_list/app/repositories/tasks/taks_repository.dart';
+import 'package:todo_list/app/repositories/tasks/taks_repository_impl.dart';
+import 'package:todo_list/app/services/tasks/task_service.dart';
+import 'package:todo_list/app/services/tasks/task_service_impl.dart';
 
-class TasksModule extends TodoListModule{
-
-  TasksModule () : super (
-    bindings: [
-       ChangeNotifierProvider(
-        create: (context) => TaskCreateController(),)
-    ],
-    routers: {
-      '/task/create': (context) => TaskCreatePage(controller: context.read())
-    }
-    
-  );
-  
+class TasksModule extends TodoListModule {
+  TasksModule()
+      : super(bindings: [
+          Provider<TasksRepository>(
+              create: (context) =>
+                  TasksRepositoryImpl(sqlLiteConnection: context.read())),
+          Provider<TaskService>(
+              create: (context) =>
+                  TaskServiceImpl(taskRepository: context.read())),
+          ChangeNotifierProvider(
+              create: (context) =>
+                  TaskCreateController(taskService: context.read()))
+        ], routers: {
+          '/task/create': (context) =>
+              TaskCreatePage(controller: context.read())
+        });
 }
